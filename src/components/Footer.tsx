85658aa7e39634fe840ed3bd5f7bcc8a8084ee49
BrandgrandRealMe <brandon@brandgrand.tech>
2024-02-27 15:33:21 -0600

    Testing some footer stuff

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import { RobowranglerLogo } from './RobowranglerLogo';
 export function Footer() {
   return (
     <footer>
-      <div style="display: flex; justify-content: center; align-items: center; height: 24px;" className="mt-8 h-24 items-center p-2">
+      <div style="display: flex; justify-content: center; align-items: center; height: 24px;" className="mt-8 items-center p-2">
         <RobowranglerLogo /> | App by: <br/> <RedHawkLogo /> 
       </div>
     </footer>
