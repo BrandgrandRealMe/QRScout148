4e1d7b4beef117fb67e4b3a01baea66b77afe8b9
BrandgrandReal <47084010+BrandgrandRealMe@users.noreply.github.com>
2024-02-27 12:05:49 -0600

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,8 +5,8 @@ export function Footer() {
   return (
     <footer>
       <div className="mt-8 flex h-24 flex-col items-center justify-center p-2">
-        <RobowranglerLogo />  <br>
-        App by: <br>
+        <RobowranglerLogo />  <br/>
+        App by: <br/>
         <RedHawkLogo /> 
       </div>
     </footer>
