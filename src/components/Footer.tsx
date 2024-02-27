a6f32cd1395f394a8c0ab884891ea9f6f1cbc1e3
BrandgrandRealMe <brandon@brandgrand.tech>
2024-02-27 15:07:00 -0600

    Even more Footer stuff

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import { RobowranglerLogo } from './RobowranglerLogo';
 export function Footer() {
   return (
     <footer>
-      <div className="mt-8 flex h-24 flex-col items-center justify-center p-2">
+      <div style=" display=flex; flex-direction=column;" className="mt-8 flex h-24 flex-col items-center justify-center p-2">
         <RobowranglerLogo /> | App by: <br/> <RedHawkLogo /> 
       </div>
     </footer>
